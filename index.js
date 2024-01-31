7253bc52ae00d9b7959955f71c5d517230c588ae
wrightbethanym <141892317+wrightbethanym@users.noreply.github.com>
2024-01-31 17:29:45 -0600

    Add files via upload
    updated index.js file

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-// import the pets array from data.js
 const pets = require('./data');
 
-// init express app
+
 const express = require('express');
 const app = express();
 
 const PORT = 8080;
 
-// GET - / - returns homepage
+
 app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
+   
 
 });
 
-// hello world route
+app.use(express.static("public"))
+
 app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
-
-// get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
-
+    res.send (pets);
 });
 
-// get pet by owner with query string
+
 app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
-
-
-    // find the pet in the pets array
+    const owner = req.query.owner;
     const pet = pets.find(pet => pet.owner === owner);
-
-    // send the pet as a response
-
+    if(pet) res.send(pet);
+    res.send("No pet with that owner found");
 });
 
-// get pet by name
+/
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
 
+    const name = req.params.name
+    console.log(pet);
+    if(pet) res.send(pet);
+    res.send("no pet found with that name");
 
-    // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
-    // send the pet as a response
 
 });
 
